93b01a144f199bd2756b8dee53f439042a220295
felixrp74 <felixrp74@gmail.com>
2022-11-27 21:13:42 -0500

    add radioViewModel

--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
@@ -2,72 +2,79 @@ package com.felixdeveloperand.radiocarabaya.ui.radio
 
 
 import android.media.AudioAttributes
-import android.media.AudioManager
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.felixdeveloperand.radiocarabaya.R
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenStarted
 import com.felixdeveloperand.radiocarabaya.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RadioFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private lateinit var mediaPlayer: MediaPlayer
-    private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001";
-    //        val url = "https://stream.radiocarabaya.com/radio/8000/radio?1586364355"
-
-
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var radioViewModel: RadioViewModel
+
+    private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001"
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val radioViewModel =
-            ViewModelProvider(this).get(RadioViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
+        radioViewModel = ViewModelProvider(this)[RadioViewModel::class.java]
 
-        mediaPlayer = MediaPlayer().apply {
-            setAudioAttributes(
-                AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .build()
-            )
-            setDataSource(url)
-            prepare() // might take long! (for buffering, etc)
-            start()
+        radioViewModel.mediaPlayer.observe(viewLifecycleOwner){
+            mediaPlayer = it
         }
 
+        lifecycleScope.launch {
+            whenStarted {
+                mediaPlayer = withContext(Dispatchers.IO) {
+                    MediaPlayer().apply {
+                        setAudioAttributes(
+                            AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .setUsage(AudioAttributes.USAGE_MEDIA)
+                                .build()
 
-        mediaPlayer.setOnPreparedListener {
-            setOnClickListeners()
+                        )
+                        setDataSource(url)
+                        prepare() // might take long! (for buffering, etc)
+                        start()
+                    }
+                }
+
+                mediaPlayer.setOnPreparedListener {
+                    setOnClickListeners()
+                }
+            }
+            // This line runs only after the whenStarted block above has completed.
+
+
         }
 
-/*
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
- */
-        return root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
 
 
@@ -94,12 +101,6 @@ class RadioFragment : Fragment() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-//        mediaPlayer.prepareAsync()
-        mediaPlayer.start()
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -114,7 +115,4 @@ class RadioFragment : Fragment() {
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-    }
 }
--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
@@ -1,13 +1,29 @@
 package com.felixdeveloperand.radiocarabaya.ui.radio
 
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class RadioViewModel : ViewModel() {
 
+
+
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+    private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001";
+
+    private var _mediaPlayer: MutableLiveData<MediaPlayer> = MutableLiveData<MediaPlayer>()
+    var mediaPlayer:LiveData<MediaPlayer> = _mediaPlayer
+
+    fun play(){
+
+
+
+    }
+
+
 }
