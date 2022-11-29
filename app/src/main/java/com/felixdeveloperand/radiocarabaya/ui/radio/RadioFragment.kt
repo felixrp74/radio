7f7c4443a4324c223991963cc2d2db08b927091f
felixrp74 <felixrp74@gmail.com>
2022-11-28 21:22:24 -0500

    still looking for perfect mediaplayer

--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
@@ -9,23 +9,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.whenStarted
-import com.felixdeveloperand.radiocarabaya.databinding.FragmentHomeBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.fragment.app.viewModels
+import com.felixdeveloperand.radiocarabaya.databinding.FragmentRadioBinding
 
 class RadioFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var _binding: FragmentRadioBinding
+//    private lateinit var mediaPlayer: MediaPlayer
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
-    private lateinit var radioViewModel: RadioViewModel
+//    private lateinit var radioViewModel: RadioViewModel
+    private val radioViewModel: RadioViewModel by viewModels()
 
     private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001"
 
@@ -35,39 +31,8 @@ class RadioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentRadioBinding.inflate(inflater, container, false)
 
-        radioViewModel = ViewModelProvider(this)[RadioViewModel::class.java]
-
-        radioViewModel.mediaPlayer.observe(viewLifecycleOwner){
-            mediaPlayer = it
-        }
-
-        lifecycleScope.launch {
-            whenStarted {
-                mediaPlayer = withContext(Dispatchers.IO) {
-                    MediaPlayer().apply {
-                        setAudioAttributes(
-                            AudioAttributes.Builder()
-                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                                .setUsage(AudioAttributes.USAGE_MEDIA)
-                                .build()
-
-                        )
-                        setDataSource(url)
-                        prepare() // might take long! (for buffering, etc)
-                        start()
-                    }
-                }
-
-                mediaPlayer.setOnPreparedListener {
-                    setOnClickListeners()
-                }
-            }
-            // This line runs only after the whenStarted block above has completed.
-
-
-        }
 
         return binding.root
     }
@@ -75,25 +40,30 @@ class RadioFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        radioViewModel.loading.observe(viewLifecycleOwner){ visible ->
+            binding.pbCircular.visibility = if (visible) View.VISIBLE else View.GONE
+        }
 
-
+//        radioViewModel.mediaPlayer.observe(viewLifecycleOwner){
+//
+//        }
 
     }
 
     private fun setOnClickListeners() {
         binding.buttonPlay.setOnClickListener {
 //            mediaPlayer.prepareAsync()
-            mediaPlayer.start()
+//            mediaPlayer.start()
             Toast.makeText(activity, "Play Buscando Radio || Reproduciendo... ", Toast.LENGTH_SHORT).show()
         }
 
         binding.buttonPause.setOnClickListener {
 //            mediaPlayer.prepareAsync()
-            mediaPlayer.pause()
+//            mediaPlayer.pause()
             Toast.makeText(activity, "En Pausa...", Toast.LENGTH_SHORT).show()
         }
         binding.buttonCloseApp.setOnClickListener {
-            mediaPlayer.stop()
+//            mediaPlayer.stop()
             activity?.apply {
                 moveTaskToBack(true)
                 finish()
@@ -103,16 +73,16 @@ class RadioFragment : Fragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+//        _binding = null
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.stop()
-        activity?.apply {
-            moveTaskToBack(true)
-            finish()
-        }
+//        mediaPlayer.stop()
+//        activity?.apply {
+//            moveTaskToBack(true)
+//            finish()
+//        }
     }
 
 }
--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
@@ -2,9 +2,10 @@ package com.felixdeveloperand.radiocarabaya.ui.radio
 
 import android.media.AudioAttributes
 import android.media.MediaPlayer
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RadioViewModel : ViewModel() {
 
@@ -16,10 +17,57 @@ class RadioViewModel : ViewModel() {
     val text: LiveData<String> = _text
     private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001";
 
-    private var _mediaPlayer: MutableLiveData<MediaPlayer> = MutableLiveData<MediaPlayer>()
-    var mediaPlayer:LiveData<MediaPlayer> = _mediaPlayer
+    private val _mediaPlayer: MutableLiveData<MediaPlayer> = MutableLiveData<MediaPlayer>()
+    val mediaPlayer:LiveData<MediaPlayer> = _mediaPlayer
 
-    fun play(){
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> get() = _loading
+
+    init {
+
+        viewModelScope.launch {
+            _loading.value = true
+            val mediaPlayer2 = withContext(Dispatchers.IO) {
+                MediaPlayer().apply {
+                    setAudioAttributes(
+                        AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+                    )
+                    setDataSource(url)
+                    prepare()
+                    start()
+
+                }
+            }
+            _mediaPlayer.value = mediaPlayer2
+            _loading.value = false
+
+        }
+
+//        viewLi .launch {
+//            whenStarted {
+//                mediaPlayer = withContext(Dispatchers.IO) {
+//                    MediaPlayer().apply {
+//                        setAudioAttributes(
+//                            AudioAttributes.Builder()
+//                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+//                                .setUsage(AudioAttributes.USAGE_MEDIA)
+//                                .build()
+//
+//                        )
+//                        setDataSource(url)
+//                        prepare() // might take long! (for buffering, etc)
+//                        start()
+//                    }
+//                }
+//
+//                mediaPlayer.setOnPreparedListener {
+//                    setOnClickListeners()
+//                }
+//            }
+        // This line runs only after the whenStarted block above has completed.
 
 
 
