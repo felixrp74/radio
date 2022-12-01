5cd094b2822ee08fc4a2b5f86e8fb06f2f3cad75
felixrp74 <felixrp74@gmail.com>
2022-12-01 18:41:03 -0500

    make a coroutine inside radioViewModel

--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioFragment.kt
@@ -22,16 +22,11 @@ class RadioFragment : Fragment() {
 //    private lateinit var mediaPlayer: MediaPlayer
 
     // This property is only valid between onCreateView and onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding
 
-//    private lateinit var radioViewModel: RadioViewModel
     private val radioViewModel: RadioViewModel by viewModels()
 
-    private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001"
-
-    var isPlaying:Boolean = false
     var isStreaming:Boolean = false
-    var player: MediaPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -47,86 +42,21 @@ class RadioFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-/*
+
         radioViewModel.loading.observe(viewLifecycleOwner){ visible ->
             binding.pbCircular.visibility = if (visible) View.VISIBLE else View.GONE
         }
 
- */
-
         binding.buttonPlay.setOnClickListener {
 
             isStreaming = if (isStreaming) {
-                stopPlaying()
+                radioViewModel.stopPlaying()
                 false
             } else {
-                startAudioStream(RADIO_URL)
+                radioViewModel.startAudioStream()
                 true
             }
         }
-    }
-
-    fun startAudioStream(url: String) {
-        if (player == null) player = MediaPlayer()
-        binding.pbCircular.visibility = View.VISIBLE
-        try {
-            Log.d("mylog", "Playing: $url")
-            player!!.setAudioStreamType(AudioManager.STREAM_MUSIC)
-            player!!.setDataSource(url)
-            //descriptor.close();
-            player!!.prepare()
-            player!!.setVolume(1f, 1f)
-            player!!.isLooping = false
-            player!!.start()
-            binding.pbCircular.visibility = View.GONE
-        } catch (e: Exception) {
-            binding.pbCircular.visibility = View.GONE
-            Log.d("mylog", "Error playing in SoundHandler: $e")
-        }
-    }
-
-    private fun stopPlaying() {
-        if (player != null && player!!.isPlaying) {
-            player!!.stop()
-            player!!.release()
-            player = MediaPlayer()
-            player!!.reset()
-        }
-    }
-
-    private fun setOnClickListeners() {
-        binding.buttonPlay.setOnClickListener {
-//            mediaPlayer.prepareAsync()
-//            mediaPlayer.start()
-            Toast.makeText(activity, "Play Buscando Radio || Reproduciendo... ", Toast.LENGTH_SHORT).show()
-        }
-
-        binding.buttonPause.setOnClickListener {
-//            mediaPlayer.prepareAsync()
-//            mediaPlayer.pause()
-            Toast.makeText(activity, "En Pausa...", Toast.LENGTH_SHORT).show()
-        }
-        binding.buttonCloseApp.setOnClickListener {
-//            mediaPlayer.stop()
-            activity?.apply {
-                moveTaskToBack(true)
-                finish()
-            }
-        }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-//        _binding = null
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-//        mediaPlayer.stop()
-//        activity?.apply {
-//            moveTaskToBack(true)
-//            finish()
-//        }
     }
 
 }
--- app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
+++ app/src/main/java/com/felixdeveloperand/radiocarabaya/ui/radio/RadioViewModel.kt
@@ -2,20 +2,20 @@ package com.felixdeveloperand.radiocarabaya.ui.radio
 
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.*
+import com.felixdeveloperand.radiocarabaya.utils.RADIO_URL
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class RadioViewModel : ViewModel() {
 
-
-
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
-    private val url = "https://24943.live.streamtheworld.com/CRP_LI_SC?csegid=20001&dist=20001&ttag=20001";
 
     private val _mediaPlayer: MutableLiveData<MediaPlayer> = MutableLiveData<MediaPlayer>()
     val mediaPlayer:LiveData<MediaPlayer> = _mediaPlayer
@@ -23,55 +23,48 @@ class RadioViewModel : ViewModel() {
     private val _loading = MutableLiveData(false)
     val loading: LiveData<Boolean> get() = _loading
 
-    init {
+    fun startAudioStream() {
 
         viewModelScope.launch {
             _loading.value = true
-            val mediaPlayer2 = withContext(Dispatchers.IO) {
-                MediaPlayer().apply {
-                    setAudioAttributes(
-                        AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                            .setUsage(AudioAttributes.USAGE_MEDIA)
-                            .build()
-                    )
-                    setDataSource(url)
-                    prepare()
-                    start()
-
+            try {
+                val mediaPlayer2 = withContext(Dispatchers.IO) {
+                    MediaPlayer().apply {
+                        setAudioAttributes(
+                            AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .setUsage(AudioAttributes.USAGE_MEDIA)
+                                .build()
+                        )
+                        setDataSource(RADIO_URL)
+                        prepare()
+                        setVolume(1f, 1f)
+                        isLooping = false
+                        start()
+                    }
                 }
+
+                _mediaPlayer.value = mediaPlayer2
+
+            }catch (e:Exception){
+
+                Log.d("mylog", "Error playing in SoundHandler: $e")
             }
-            _mediaPlayer.value = mediaPlayer2
+
             _loading.value = false
 
         }
 
-//        viewLi .launch {
-//            whenStarted {
-//                mediaPlayer = withContext(Dispatchers.IO) {
-//                    MediaPlayer().apply {
-//                        setAudioAttributes(
-//                            AudioAttributes.Builder()
-//                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-//                                .setUsage(AudioAttributes.USAGE_MEDIA)
-//                                .build()
-//
-//                        )
-//                        setDataSource(url)
-//                        prepare() // might take long! (for buffering, etc)
-//                        start()
-//                    }
-//                }
-//
-//                mediaPlayer.setOnPreparedListener {
-//                    setOnClickListeners()
-//                }
-//            }
-        // This line runs only after the whenStarted block above has completed.
-
-
-
     }
 
+
+    fun stopPlaying() {
+        if ( _mediaPlayer.value!!.isPlaying) {
+            _mediaPlayer.value!!.stop()
+            _mediaPlayer.value!!.release()
+            _mediaPlayer.value = MediaPlayer()
+            _mediaPlayer.value!!.reset()
+        }
+    }
 
 }
